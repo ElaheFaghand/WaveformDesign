ade01d8df494225d517cf0dd24d34e88bb23aeed
Elahe Faghand <132060865+ElaheFaghand@users.noreply.github.com>
2024-10-22 09:45:24 +0200

    Delete New folder directory

--- New folder/shift_R_construction.m
+++
@@ -1,7 +0,0 @@
-function R_shift = shift_R_construction (of,N,R)
-    A = zeros(N);
-        for i=1:N
-            A(i,i)= exp(1i*(pi)*(i-1)*(sin(of*pi/180)));
-        end
-    R_shift=A*R*A';
-end
