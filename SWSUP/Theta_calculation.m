41184af4b1567265d03015a48c086ffe1de6056c
Elahe Faghand <132060865+ElaheFaghand@users.noreply.github.com>
2024-10-23 13:28:50 +0100

    Update Theta_calculation.m

--- SWSUP/Theta_calculation.m
+++ SWSUP/Theta_calculation.m
@@ -1,9 +1,7 @@
+% Shift Evaluation
 function  [eta, sum_Beta_i_P_i] = Theta_calculation (gamma, P_d , M, eta, beta)
     K = length(gamma);
     rc = .8;
-%     w = 1; 
-     %% 
-%      for l = 1 : length(eta) 
          numerator = 1;  
              for w =1:100
 %               while abs(numerator) > 1e-5
@@ -21,18 +19,6 @@ function  [eta, sum_Beta_i_P_i] = Theta_calculation (gamma, P_d , M, eta, beta)
                   denumerator =  beta' .*( ones(1,K)* rond1.^2 ) +  ((sum_Beta_i_P_i' - P_d) * rond2);
                   eta = eta - rc * numerator./denumerator;
 %                w = w+1;
-              end
-%      end
-     %%
-%     for j = 1 : length(eta) 
-%         Rss(:,:,j) = shift_R_construction (eta(j), M, R_0);
-%         P_R_ss(:,j) = Beam_Pattern (M, gamma,Rss(:,:,j));
-%         plot(gamma , 10*log10(abs(P_R_ss(:,j)))), hold on, grid on, axis([-1, 1, -40,1]);
-%         P_R_ss_weight(:,j) = beta(j) * P_R_ss(:,j); 
-%     end
-%     P_R_total = sum(P_R_ss_weight,2);
+             end
   sum_Beta_i_P_i =  P_shift * beta;   
-
-%    plot(gamma , 10*log10(abs(sum_Beta_i_P_i))), hold on, grid on, axis([-1, 1, -40,1]);
-
 end
