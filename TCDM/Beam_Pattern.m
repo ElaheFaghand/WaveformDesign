a773afc144f3559cad6e977c1dfd2cdf9ee6cf6e
Elahe Faghand <132060865+ElaheFaghand@users.noreply.github.com>
2024-10-23 12:19:10 +0100

    Delete TCDM/Beam_Pattern.m

--- TCDM/Beam_Pattern.m
+++
@@ -1,7 +0,0 @@
-function Beam_Pattern = Beam_Pattern (N,gamma,R)
-Beam_Pattern = zeros(1, length(gamma));
-    for l = 1:length(gamma)
-        et = exp(1j*pi*(0:N-1)*(gamma(l))).';
-        Beam_Pattern(l) = et'*R*et;
-    end
-end
