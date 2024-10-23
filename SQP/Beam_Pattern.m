db24a84b0a6a88f7308d788b7c508a6dbc818eb3
Elahe Faghand <132060865+ElaheFaghand@users.noreply.github.com>
2024-10-23 12:07:18 +0100

    Delete SQP/Beam_Pattern.m

--- SQP/Beam_Pattern.m
+++
@@ -1,7 +0,0 @@
-function Beam_Pattern = Beam_Pattern (N,theta,R)
-Beam_Pattern = zeros(1, length(theta));
-    for l = 1:length(theta)
-        et = exp(1j*pi*(0:N-1)*sin(theta(l))).';
-        Beam_Pattern(l) = et'*R*et;
-    end
-end
