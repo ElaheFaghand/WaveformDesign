af37b6d117de9b45b7c64963f636df275486d399
Elahe Faghand <132060865+ElaheFaghand@users.noreply.github.com>
2024-10-23 12:05:59 +0100

    SQP_SLL_wideband, Narrow band, Matching.m

--- SQP/SQP_SLL_wideband.m
+++ SQP/SQP_SLL_wideband.m
@@ -1,42 +1,24 @@
-% Stoica onProbing Min SLL
+%% Stoica 2007 SQP Method for solving SLL design problem in section III-D for wide band beampatterns in article: 
+%% On Probing Signal Design For MIMO Radar
+%% It is written by Elahe Faghand
 clc
 clear all
 tic
 theta = (-90:1:90)'*(pi/180);
 L = length(theta);
-
-%% Fig 3a JianLi
-N = 16;
+N = 16; % Number of antennas 
+%% desired Beam pattern angles
 teta0 = 0; 
 teta1 = -20 - 3;
 teta2 = 20 + 3;
 teta = [teta1+3:1:teta2-3];
-% multi lobe
-% teta1 = 35 - 3;
-% teta2 = 55 + 3;
-% teta_2 = [teta1+3:1:teta2-3];
-% 
-% teta1 = -55 - 3;
-% teta2 = -35 + 3;
-% teta_3 = [teta1+3:1:teta2-3];
-%  
-% teta = [teta teta_2 teta_3];
-%%
 omega1 = [-90:1:-28];
 omega2 = [28:1:90];
 omega = [omega1,omega2];
-
-% multi lobe
-
-% omega1 = [-90:1:-61];
-% omega2 = [-29:1:-16];
-% omega3 = [16:1:29];
-% omega4 = [61:1:90];
-% % 
-% omega = [omega1 omega2 omega3 omega4];
 L1 = length(omega);
 
 %% SQP Method
+% steering vector definitons
 A = zeros(L1,N^2);
 aT = exp( 1j * pi*(0:N-1)*sin(teta0*(pi/180))).';
 A00 = kron(aT.',aT');
@@ -46,7 +28,7 @@ aT = exp( 1j * pi*(0:N-1)*sin(teta2*(pi/180))).';
 A2 = kron(aT.',aT');
 
 cvx_begin quiet
-variable R(N,N) complex 
+variable R(N,N) complex  % covariance matrix
 variable t 
 minimize(-t)
 subject to
@@ -59,7 +41,6 @@ for k=1:length (teta)
         aT = exp(  1j * pi*(0:N-1)*sin(omega(l)*pi/180) ).';
         A(l,:) = kron(aT.',aT');
         SS(l) = A0(k,:) * vec(R) - A(l,:)*vec(R) - t;
-        % SS(l)=A0(k,:) *vec(R)-A(l,:)*vec(R)*abs(2e2*(omega(l)))^0.005-t;
     end
     real(SS) >= 0;
     k;
@@ -68,6 +49,14 @@ A1*vec(R)-0.5*A00*vec(R) == 0;  % for multi lobes i removed this line
 A2*vec(R)-0.5*A00*vec(R) == 0;  
 cvx_end
 toc
-R=  R/sum(sum(R));
-P_sqp = Beam_Pattern(N, theta, R);
+R =  R/sum(sum(R));
+P_sqp = Beam_Pattern(N, theta, R); % achieved beampattern
 plot(theta*180/pi ,10*log10(abs(P_sqp))),grid on,hold on
+%%
+function Beam_Pattern = Beam_Pattern (N,theta,R)
+Beam_Pattern = zeros(1, length(theta));
+    for l = 1:length(theta)
+        et = exp(1j*pi*(0:N-1)*sin(theta(l))).';
+        Beam_Pattern(l) = et'*R*et;
+    end
+end
